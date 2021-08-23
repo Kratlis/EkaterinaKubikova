7e97aace4d1bfbe22c3078731c3c316d5fce8b0a
Ekaterina Kubikova <kwork@fduch.ru>
2021-08-24 01:32:14 +0300

    utils classes combined into one, common test logic moved into HomePageTest

--- src/test/java/com/epam/tc/hw2/ex1/IndexPageTest.java
+++ src/test/java/com/epam/tc/hw2/ex1/IndexPageTest.java
@@ -1,52 +1,32 @@
 package com.epam.tc.hw2.ex1;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.epam.tc.hw2.HomePageTest;
+import com.epam.tc.hw2.Utils;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HomePageTest {
-
-    private WebDriverWait wait;
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void prepareDriver() {
-        driver = Utils.getChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-    }
-
-    // 12. Close Browser
-    @AfterMethod
-    private void close() {
-        driver.quit();
-    }
+public class IndexPageTest extends HomePageTest {
 
     @Test
-    public void testHomePage() {
+    public void testIndexPage() {
         SoftAssertions softAssert = new SoftAssertions();
 
         // 1. Open test site by URL
-        openSite();
+        openHomePage();
 
         // 2. Assert Browser title
-        pageOpenTest(softAssert);
+        homePageOpenTest(softAssert);
 
         // 3. Perform login
         login();
 
-        // 4. Assert Username is logged
+        // 4. Assert User name in the left-top side of screen that user is logged
         loginTest(softAssert);
 
         // 5. Assert that there are 4 items on the header section are displayed,
@@ -70,41 +50,6 @@ public class HomePageTest {
         //     and they have proper text
         leftSectionItemsTest(softAssert);
         softAssert.assertAll();
-    }
-
-    private void openSite() {
-        driver.navigate().to(Utils.HOME_PAGE_URL);
-    }
-
-    private void pageOpenTest(SoftAssertions softAssert) {
-        softAssert.assertThat(driver.getCurrentUrl()).isEqualTo(Utils.HOME_PAGE_URL);
-        softAssert.assertThat(driver.getTitle()).isEqualTo(Utils.HOME_PAGE_TITLE);
-    }
-
-    private void login() {
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
-            By.className("profile-photo")
-        ));
-        loginButton.click();
-        WebElement loginField = wait.until(ExpectedConditions.presenceOfElementLocated(
-            By.id("name")
-        ));
-        loginField.sendKeys(Utils.LOGIN);
-        WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(
-            By.id("password")
-        ));
-        passwordField.sendKeys(Utils.PASSWORD);
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(
-            By.id("login-button")
-        ));
-        submitButton.click();
-    }
-
-    private void loginTest(SoftAssertions softAssert) {
-        String actualUsername = wait.until(ExpectedConditions.presenceOfElementLocated(
-            By.id("user-name"))
-        ).getText();
-        softAssert.assertThat(actualUsername).isEqualTo(Utils.USERNAME);
     }
 
     private void headerSectionItemsTest(SoftAssertions softAssert) {
@@ -171,6 +116,6 @@ public class HomePageTest {
         List<String> leftSectionItemsTexts =
             leftSectionItems.stream().map(WebElement::getText).collect(Collectors.toList());
         softAssert.assertThat(leftSectionItemsTexts)
-            .containsExactlyElementsOf(Utils.leftSectionItemsTexts);
+                  .containsExactlyElementsOf(Utils.leftSectionItemsTexts);
     }
 }
