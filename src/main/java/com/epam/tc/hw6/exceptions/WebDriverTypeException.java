7a07c08b0406665b5692aeafa7f86dd700e7e038
Ekaterina Kubikova <kwork@fduch.ru>
2021-09-10 11:53:29 +0300

    merged with main and deleted previous homeworks

--- src/main/java/com/epam/tc/hw6/exceptions/WebDriverTypeException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.tc.hw6.exceptions;
-
-public class WebDriverTypeException extends RuntimeException {
-
-    public WebDriverTypeException(String message) {
-        super(message);
-    }
-}
